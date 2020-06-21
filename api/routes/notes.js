2c0baec8abf14172d51494f2cdd99ca442b5d647
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2020-06-22 01:12:15 +0600

    Removed unnecessary routes

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -1,12 +1,10 @@
 const { Router } = require('express');
-const { index, store, show, update, destroy } = require('../controllers/notes');
+const { index, store, destroy } = require('../controllers/notes');
 
 const router = Router();
 
 router.get('/', index);
 router.post('/', store);
-router.get('/:id', show);
-router.put('/:id', update);
 router.delete('/:id', destroy);
 
 module.exports = router;
